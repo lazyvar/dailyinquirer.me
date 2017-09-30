f5c77aa29758870c21d1a6cc14b45ab303b4a5c0
Mack Hasz <mack@hasz.email>
2017-09-30 13:09:29 -0400

    prompt data model

--- dailyinquirer/core/models.py
+++ dailyinquirer/core/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from authentication.models import User
 
 
+class Prompt(models.Model):
+    question = models.CharField(max_length=255)
+    mail_day = models.DateTimeField()
+    override_html = models.TextField(null=True)
+
+    def __str__(self):
+        return self.question
+
+
 class Entry(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    prompt = models.ForeignKey(Prompt, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.content[:16]
