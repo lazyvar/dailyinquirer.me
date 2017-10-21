3c89aa1e0c30a6f8909beb4f03642f443a57318a
Mack Hasz <mack@hasz.email>
2017-10-21 12:40:08 -0400

    files for heroku

--- dailyinquirer/dailyinquirer/settings/prod.py
+++ dailyinquirer/dailyinquirer/settings/prod.py
@@ -1,15 +1,23 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['dailyinquirer.me']
+ALLOWED_HOSTS = ['dailyinquirer.me', 'www.dailyinquirer.me']
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
