861a1bbd48f6426d43042a86d08d742afe6acabc
Mack Hasz <mack@hasz.email>
2017-11-12 16:07:08 -0500

    added about section to splash

--- dailyinquirer/core/management/commands/send_daily_mail.py
+++ dailyinquirer/core/management/commands/send_daily_mail.py
@@ -7,7 +7,7 @@ from core.utils import mail_newsletter
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for user in User.objects.all():
+        for user in User.objects.filter(confirmed_email=True):
             local_time = user.local_time()
             if local_time is not None and local_time.hour == 5:
                 mail_newsletter(user)
