dbf0a84728a2521e4bce0549fedcabf20ebbcc37
Mack Hasz <mack@hasz.email>
2017-12-03 15:46:50 -0500

    moar js hax to fix mailgun <br> formatting

--- dailyinquirer/dailyinquirer/static/js/hack.js
+++ dailyinquirer/dailyinquirer/static/js/hack.js
@@ -1,11 +1,13 @@
-
-
 window.onload = function() {
 	let headers = document.querySelectorAll("h3")
-
 	for (header of headers) {
-		console.log(header)
 		let innerHtml = header.innerHTML
 		header.innerHTML= innerHtml.replace(/\./g, "<span class=\"hack\">.</span>")
+	}
+
+	let main = document.getElementsByClassName("main")
+	for (row of main) {
+		let innerHtml = row.innerHTML
+		row.innerHTML = innerHtml.replace(/<br>/g, " ")
 	}
 }
